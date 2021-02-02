e4364e5c14f1a5ddc8ec866da0466c0e68ee6056
shaozhiqi <zhiqi.shao@foxmail.com>
2021-02-02 17:08:39 +0800

    Update MyExecutor.java

--- src/com/unicss/MyExecutor.java
+++ src/com/unicss/MyExecutor.java
@@ -28,8 +28,8 @@ public static void main(String args[]){
     service.shutdown();
 }
       /**
-     * 1.创建了一个可重用的固定线程池，池中有四个线程(无边界队列方式运行，就是4个线程都启用，不留备用)。
-     * 2.循环，提交了10个多线程任务
+     * 1.创建了一个可重用的固定线程池，池中有四个线程
+     * 2.循环，提交了10个多线程任务(“无界队列里”。这是一个有序队列（BlockingQueue))。
      * 3.10个任务中随机选了4个分给了池子中的4个线程
      * 4.4个线程运行任务，任务2/0/1/3并行运行，随机睡了，2和0睡的时间长，所以醒的晚。
      * 5.任务1睡的最短，所以醒的最早，最早执行结束。结束后就有一个线程空闲出来，可以看到任务4得到了该空闲线程，开始执行任务。以此类推。。。
@@ -57,6 +57,20 @@ public static void main(String args[]){
      * [5] end.
      * [6] end.
      *
+     *
+     *         int corePoolSize,
+     *         int maximumPoolSize,
+     *         long keepAliveTime,
+     *         TimeUnit unit,
+     *         BlockingQueue<Runnable> workQueue,
+     *         ThreadFactory threadFactory,
+     *         RejectedExecutionHandler handler
+     * ExecutorService service = new ThreadPoolExecutor(2, 4, 5000L, TimeUnit.MILLISECONDS,
+     *                 new LinkedBlockingQueue<Runnable>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+     *
+     * 另外它使用了Executors的静态函数生成一个固定的线程池，顾名思义，线程池的线程是不会释放的，即使它是Idle。
+     * 这就会产生性能问题，比如如果线程池的大小为200，当全部使用完毕后，所有的线程会继续留在池中，相应的内存和线程切换（while(true)+sleep循环）都会增加。
+     * 如果要避免这个问题，就必须直接使用ThreadPoolExecutor()来构造。可以像通用的线程池一样设置“最大线程数”、“最小线程数”和“空闲线程keepAlive的时间”。
      */
 }
 
